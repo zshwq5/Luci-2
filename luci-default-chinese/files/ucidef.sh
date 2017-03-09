6579f49d7d6b095bc7caf655678f3d63544a3788
cpexplorer <jc327134435@gmail.com>
2017-03-09 18:35:22 +0800

    set timezone

--- luci-default-chinese/files/ucidef.sh
+++ luci-default-chinese/files/ucidef.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 uci batch <<-EOF
 	set luci.main.lang=zh_cn
-	set luci.main.mediaurlbase=/luci-static/material
+	set luci.languages.zh_cn=ÆÕÍ¨»° (Chinese)
+	set system.@system[0].zonename=Asia/Shanghai
+	commit system
 	commit luci
 EOF
